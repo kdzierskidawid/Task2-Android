b4dbc27b7d1ff9ba1a2dd50b6f9f433ddf9ba21b
kdzierskidawid <dawid1kedzierski@gmail.com>
2020-01-27 17:53:40 +0100

    timer and marker's list init

--- app/src/main/java/com/example/task2/MapsActivity.kt
+++ app/src/main/java/com/example/task2/MapsActivity.kt
@@ -10,18 +10,24 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
     private lateinit var searchView: SearchView
+    private lateinit var markerList: ArrayList<Marker>
+    private lateinit var timer: Timer
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         initLayout()
+        initValues()
     }
 
 
@@ -39,5 +45,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    fun initValues(){
+        markerList = ArrayList()
+        timer = Timer()
     }
 }
